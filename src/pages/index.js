f0aba10e9bac39838a9fdf458315a6dc54c67a00
office PC <teddywijaya93@gmail.com>
2023-02-20 15:17:12 +0700

    add favicon and update meta

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,6 +25,11 @@ export default function Home() {
 				<meta name="description" content="Jadoo App Travel" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<link rel="icon" href="/favicon.ico" />
+				<meta property="og:url" content="https://slice-ui-next.vercel.app" />
+				<meta property="og:type" content="website" />
+				<meta property="og:title" content="Jadoo" />
+				<meta property="og:description" content="travel app, life app, aplikasi jalan-jalan, pesan guide, dan sebagainya" />
+				<meta property="og:image" content="https://slice-ui-next.vercel.app/aplikasi-travel-online.jpg" />
 			</Head>
 
 			<Header />
